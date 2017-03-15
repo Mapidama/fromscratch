adc4edf6700229d95c44910bcd67342608d0675f
Paul A <delentrenor@gmail.com>
2017-03-15 19:07:26 +0100

    added bootstrap

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express=require("express");
 var app= express();
 
+app.use(express.static("static"));
+
 app.get("/hello",function(req,res) {
   res.send("hello World");
 });
+
+
 
 app.listen(9500,function(err){
   console.log("Application started on port 9500 !");
